c1b0898bea5bc6cdc7b147339ff8c08ab821e936
Nicollas Vilela <162446919+NicollasVilela@users.noreply.github.com>
2025-03-10 19:07:45 -0300

    Create Pilha.java

---
+++ exercSem03/Pilha.java
@@ -0,0 +1,39 @@
+class Pilha {
+    private int topo;
+    private int[] elementos;
+    private int capacidade;
+
+    public Pilha(int capacidade) {
+        this.capacidade = capacidade;
+        this.elementos = new int[capacidade];
+        this.topo = -1;  // Indica que a pilha está vazia
+    }
+
+    public boolean isEmpty() {
+        return topo == -1;
+    }
+
+    public boolean isFull() {
+        return topo == capacidade - 1;
+    }
+
+    public void push(int elemento) {
+        if (isFull()) {
+            System.out.println("Erro: Pilha cheia!");
+            return;
+        }
+        elementos[++topo] = elemento;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Erro: Pilha vazia!");
+            return -1; // Indicativo de erro
+        }
+        return elementos[topo--];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Erro: Pilha vazia!");
+        
